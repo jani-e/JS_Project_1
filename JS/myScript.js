7c67f22e3ea5b1303c0c1abf6c598bfb0f5d3eb5
jani-e <j.eriksson91@gmail.com>
2021-10-02 11:26:18 +0300

    basic layout + add-function

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,3 +1,8 @@
-function helloWorld() {
-    console.log("Hello World!");
+function addRow() {
+    var list = document.getElementById("listItems"); //adds listItems element to variable called list
+    var li = document.createElement("li"); //create a new html element called li
+    var addValue = "text"; // @@@@replace with fetched value & document@@@@
+    var liNode = document.createTextNode(addValue); //create a new text node from given data
+    li.appendChild(liNode); //add text node to previously made new li element
+    list.appendChild(li); //add new li element with data to the existing list element
 }
