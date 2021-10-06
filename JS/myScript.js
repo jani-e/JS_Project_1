9b19e1e30a33e33deaf9c231d0b98d5aa7ad48de
jani-e <j.eriksson91@gmail.com>
2021-10-06 21:50:18 +0300

    changed to form todo-list

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,8 +1,36 @@
-function addRow() {
+var todoList = []; //stores the list in an array
+
+function addTodoItem(event) {
+    event.preventDefault(); //stops form from refreshing the page
+    var formValue = document.forms["todoForm"]["todoInput"].value; //user given value in the form to submit
+    if (formValue == "") { //prevents adding empty items to the list
+        return false;
+    }
+    todoList.push(formValue); //adds user value to the array list
+
+    var list = document.getElementById("name"); //returns todo list element in variable
+    list.children[0].innerHTML += "<li>" + todoList[todoList.length-1]+"</li>"; // @@@ remake?
+    // @@@ change from div name to ul todolist?
+}
+
+function addRowOld() {
     var list = document.getElementById("listItems"); //adds listItems element to variable called list
     var li = document.createElement("li"); //create a new html element called li
-    var addValue = "text"; // @@@@replace with fetched value & document@@@@
+
+    var formValue = document.forms["todoForm"]["todoInput"].value;
+    //console.log(value);
+
+    var addValue = formValue // @@@@replace with fetched value & document@@@@
     var liNode = document.createTextNode(addValue); //create a new text node from given data
     li.appendChild(liNode); //add text node to previously made new li element
     list.appendChild(li); //add new li element with data to the existing list element
+}
+
+function validateForm() {
+    var formValue = document.forms["todoForm"]["todoInput"].value;
+    if (formValue == "" || formValue == null) {
+        alert("false") //testing
+        return false;
+    }
+    console.log("true") //testing
 }
