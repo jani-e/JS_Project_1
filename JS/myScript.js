1508c90a64f25358cf454f809a23983ed320ef2a
jani-e <j.eriksson91@gmail.com>
2021-10-09 10:00:14 +0300

    commenting

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,26 +1,26 @@
 var todoArray = []; //stores the list in an array
-var formInput = document.forms["todoForm"]["todoInput"];
+var formInput = document.forms["todoForm"]["todoInput"]; //variable to access form input element to get value from it later in other functions
 
-function addTodoItem() {
+function addTodoItem() { //function to add form value to the todoarray and localstorage
     var formValue = formInput.value; //user given value in the form to submit
     todoArray.push(formValue); //adds value to the array list
-    saveLocalStorage(todoArray);
+    saveLocalStorage(todoArray); //adds current form array to the localstorage function
     todoInput.value = ""; //changes input field back to empty
 }
 
 function validateForm(event) { //validates form entry and if true activates function addTodoItem
     event.preventDefault(); //stops the form from refreshing page on submit
-    var formValue = formInput.value;
+    var formValue = formInput.value; //user given value in the form to submit
     if (formValue == "" || formValue == null || formValue.length < 3) { //checks if given value is empty or its character length is shorter than 4
         alert("Task can't be empty or shorter than 3 characters!") //greets user with error message
         // @@@@@ todo: add error highlight to textfield
         todoInput.value = ""; //changes input field back to empty
         return false; //stops form from continuing
     }
-    addTodoItem();
+    addTodoItem(); //after validation calls function to add form value to the todoarray and localstorage
 }
 
-function printArrayToHtml(array) {
+function printArrayToHtml(array) { //function to print todo list items on the page from given array parameter
     var ul = document.getElementById("todoItems"); // access ul element in html
     ul.innerHTML = ""; //empty existing ul so the page doesn't duplicate array li values on the page
     for (let index = 0; index < array.length; index++) { //loop through array
@@ -31,22 +31,22 @@ function printArrayToHtml(array) {
     }
 }
 
-function saveLocalStorage(todoArray) {
-    localStorage.setItem("savedArray", JSON.stringify(todoArray));
-    printArrayToHtml(todoArray);
+function saveLocalStorage(todoArray) { //saves given array to localstorage
+    localStorage.setItem("savedArray", JSON.stringify(todoArray)); //saves todo array to the localstorage
+    printArrayToHtml(todoArray); //print array function
 }
 
-function loadLocalStorage() {
-    var loadedData = localStorage.getItem("savedArray");
-    var array = JSON.parse(loadedData);
-    if (array == null) {
-        return;
+function loadLocalStorage() { //loads array from localstorage
+    var loadedData = localStorage.getItem("savedArray"); //retrieves data from localstorage into a variable
+    var array = JSON.parse(loadedData); //parses the retrieved data to an array
+    if (array == null) { //if array is empty, return @@@ check if this is working
+        return; //stop the function from continueing
     }
-    todoArray = array;
-    printArrayToHtml(todoArray);
+    todoArray = array; //replace initial todo array with loaded array
+    printArrayToHtml(todoArray); //print array function
 }
 
-loadLocalStorage();
+loadLocalStorage(); //load localstorage initially when opening the page
 //localStorage.removeItem("savedArray");
 
 //adding a new item resets localStorage from previoue session (after refresh), seems ok now
