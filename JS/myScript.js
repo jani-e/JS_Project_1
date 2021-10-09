765586abd0600a09a8b073a5d02d2ab8486f02d2
jani-e <j.eriksson91@gmail.com>
2021-10-09 09:34:10 +0300

    localstorage issues

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,36 +1,51 @@
-var todoList = []; //stores the list in an array
+var todoArray = []; //stores the list in an array
 
-function addTodoItem(event) {
-    event.preventDefault(); //stops form from refreshing the page
-    var formValue = document.forms["todoForm"]["todoInput"].value; //user given value in the form to submit
-    if (formValue == "") { //prevents adding empty items to the list
-        return false;
+function addTodoItem() {
+    var inputItem = document.forms["todoForm"]["todoInput"].value; //user given value in the form to submit
+    todoArray.push(inputItem); //adds value to the array list
+    saveLocalStorage(todoArray);
+    todoInput.value = ""; //changes input field back to empty
+}
+
+function validateForm(event) { //validates form entry and if true activates function addTodoItem
+    event.preventDefault(); //stops the form from refreshing page on submit
+    var inputValue = document.forms["todoForm"]["todoInput"].value;
+    if (inputValue == "" || inputValue == null || inputValue.length < 3) { //checks if given value is empty or its character length is shorter than 4
+        alert("Task can't be empty or shorter than 3 characters!") //greets user with error message
+        // @@@@@ todo: add error highlight to textfield
+        todoInput.value = ""; //changes input field back to empty
+        return false; //stops form from continuing
     }
-    todoList.push(formValue); //adds user value to the array list
-
-    var list = document.getElementById("name"); //returns todo list element in variable
-    list.children[0].innerHTML += "<li>" + todoList[todoList.length-1]+"</li>"; // @@@ remake?
-    // @@@ change from div name to ul todolist?
+    addTodoItem();
 }
 
-function addRowOld() {
-    var list = document.getElementById("listItems"); //adds listItems element to variable called list
-    var li = document.createElement("li"); //create a new html element called li
-
-    var formValue = document.forms["todoForm"]["todoInput"].value;
-    //console.log(value);
-
-    var addValue = formValue // @@@@replace with fetched value & document@@@@
-    var liNode = document.createTextNode(addValue); //create a new text node from given data
-    li.appendChild(liNode); //add text node to previously made new li element
-    list.appendChild(li); //add new li element with data to the existing list element
-}
-
-function validateForm() {
-    var formValue = document.forms["todoForm"]["todoInput"].value;
-    if (formValue == "" || formValue == null) {
-        alert("false") //testing
-        return false;
+function printArrayToHtml(array) {
+    var ul = document.getElementById("todoItems"); // access ul element in html
+    for (let index = 0; index < array.length; index++) { //loop through array
+        var li = document.createElement("li"); //create element li
+        var liNode = document.createTextNode(array[index]); //create a textnode from current index value in the array
+        li.appendChild(liNode); //add linode to li
+        ul.appendChild(li); //add new valued li to existing ul
     }
-    console.log("true") //testing
 }
+
+function saveLocalStorage(todoArray) {
+    localStorage.setItem("savedArray", JSON.stringify(todoArray));
+    printArrayToHtml(todoArray);
+}
+
+function loadLocalStorage() {
+    var loadedData = localStorage.getItem("savedArray");
+    var array = JSON.parse(loadedData);
+    if (array == null) {
+        return;
+    }
+    printArrayToHtml(array);
+}
+
+loadLocalStorage();
+//localStorage.removeItem("savedArray");
+
+//adding a new item resets localStorage from previoue session (after refresh)
+//check also loadLocalStorage if statement, related?
+
